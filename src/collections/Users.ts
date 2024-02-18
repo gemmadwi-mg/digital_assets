44100b07995d1b9dcf1ffddcb0c5a01318b225f8
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-02-18 11:18:16 +0700

    make sign up,sign in (auth)

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,7 +2,13 @@ import { CollectionConfig } from "payload/types";
 
 export const Users: CollectionConfig = {
   slug: "users",
-  auth: true,
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}>Verify aacount</a>`;
+      },
+    },
+  },
   access: {
     read: () => true,
     create: () => true,
@@ -12,7 +18,7 @@ export const Users: CollectionConfig = {
       name: "role",
       defaultValue: "user",
       required: true,
-      
+
       type: "select",
       options: [
         { label: "Admin", value: "admin" },
